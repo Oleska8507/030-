928869b32b589c8cf8ce92314e65ea5cb734b7c4
Olesya <oleska8507@mail.ru>
2024-04-19 09:52:52 +0300

    izm19/04.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+
+const Profile = () => {
+  return (
+    <h2>Это страница с профилем</h2>
+  )
+}
+const Friends = () => {
+  return (
+    <h2>Это страница с друзьями</h2>
+  )
+}
+const Shop = () => {
+  return (
+    <h2>Это магазин</h2>
+  )
+}
 
 function App() {
   return (
   <div className="container mt-5">
+    <div className="row">
     <div className="col-3">
     <div className="nav flex-column nav-pills">
-      <a href="main" className="nav-link active">Главная</a>
+      <a href="/" className="nav-link">Главная</a>
       <a href="profile" className="nav-link">Профиль</a>
       <a href="friends" className="nav-link">Друзья</a>
       <a href="shop" className="nav-link">Магазин</a>
       </div>
     </div>
     <div className="col-9">
+      <Routes>
+        <Route path='/' element={<h2>вы в личном кабинете, выбор в меню слева</h2>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/friends' element={<Friends/>}/>
+        <Route path='/shop' element={<Shop/>}/>
+      </Routes>
     <span></span>
+    </div>
     </div>
   </div>
   );
